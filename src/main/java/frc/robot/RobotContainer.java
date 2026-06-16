5711672e7c957e15ded8f7c733178885971812be
AarushSehgal-debug <aarush.sehgal.24@gmail.com>
2026-06-16 18:01:31 -0500

    IntakeChanges

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,6 +6,7 @@ package frc.robot;
 
 import frc.robot.Constants.OperatorConstants;
 import frc.robot.subsystems.IndexerSubsystem;
+import frc.robot.subsystems.IntakeSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
@@ -15,6 +16,7 @@ public class RobotContainer {
   private final CommandXboxController m_driverController = new CommandXboxController(
       OperatorConstants.kDriverControllerPort);
   private final IndexerSubsystem indexer = new IndexerSubsystem();
+  private final IntakeSubsystem intake = new IntakeSubsystem();
 
   /**
    * The container for the robot. Contains subsystems, OI devices, and commands.
@@ -23,13 +25,16 @@ public class RobotContainer {
     configureBindings();
     indexer.setDefaultCommand(new RunCommand(() -> indexer.motor.set(0), indexer));
 
-  }
+    intake.setDefaultCommand(new RunCommand(() -> intake.intakeMotor.set(0), intake));
 
+  }
+ 
   private void configureBindings() {
     // TODO: Run shooter at 20 RPS on right trigger
     // TODO: Run intake at 70% power on left trigger
     m_driverController.y().whileTrue(indexer.start());
     // TODO: Run indexer at 70% power on y
+    
     // TODO: Run kicker at 20% power on a
   }
 
