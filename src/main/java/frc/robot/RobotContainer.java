6a8241446e24875a3c7f67944720b68fb2c16180
archanaa <archushankar14@.gmail.com>
2026-06-17 12:35:26 -0500

    kicker

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,6 +6,7 @@ package frc.robot;
 
 import frc.robot.Constants.OperatorConstants;
 import frc.robot.subsystems.IndexerSubsystem;
+import frc.robot.subsystems.KickerSubsystem;
 import frc.robot.subsystems.IntakeSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.RunCommand;
