1c76c320f337292a813ffaf8aab942959140b52e
archanaa <archushankar14@.gmail.com>
2026-06-17 09:45:10 -0500

    start command on kicker

--- src/main/java/frc/robot/subsystems/KickerSubsystem.java
+++ src/main/java/frc/robot/subsystems/KickerSubsystem.java
@@ -7,5 +7,13 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class KickerSubsystem extends SubsystemBase {
-    
+    public TalonFX kickerMotor = new TalonFX(37);
+
+    public KickerSubsystem() {
+
+    }
+
+    public Command start() {
+        return run(() -> kickerMotor.set(0.2));
+    }
 }
